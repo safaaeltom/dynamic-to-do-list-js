d820cc3152d4375d1b308a90b46798d1be20760f
Safaa <safaaeltom@gmail.com>
2025-12-03 20:47:29 +0300

    added to do list javascript file

---
+++ script.js
@@ -0,0 +1,39 @@
+// setup event listener for page load
+document.addEventListener('DOMContentLoaded', function() {
+    addTask();
+
+// select the DOM elements
+    const addButton = document.getElementById('AddTask');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+// create the addtask function
+    function addTask() {
+        let taskText = taskInput.value.trim();
+        if (taskText == "") {
+            alert("Please enter a task");
+
+// task creation and removal        
+        } else {
+            let list = document.createElement("li");
+            list.textContent = taskText;
+
+            let btn = document.createElement("button");
+            btn.textContent = "Remove";
+            btn.classList.add("remove-button");
+            btn.onclick = function() {
+                list.remove();
+            };
+                list.appendChild(btn)
+                taskList.appendChild(list);
+                taskInput.value = "";
+            }
+            addButton.addEventListener("click", addTask);
+            taskInput.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter') {
+                    addTask();
+               }
+            }
+        );
+    };
+});
